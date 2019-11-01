ed5f0c9ac98a6b1976a077d33d7733f63089c06a
Marcos Mercedes <marcos.mercedesn@gmail.com>
2019-11-01 12:55:02 -0400

    Add filter by api key client

--- lib/ClprApi/listings/filter_builder_by_api_key.rb
+++ lib/ClprApi/listings/filter_builder_by_api_key.rb
@@ -3,22 +3,18 @@ module ClprApi
     class FilterBuilderByApiKey
       SEARCH_CONDITIONS_KEYS = ["lister", "business"].freeze
 
-      attr_reader :key
+      attr_reader :api_key
 
-      def initialize(key)
-        @key = key
+      def initialize(api_key)
+        @api_key = api_key
       end
 
-      def api_key
-        @api_key ||= ApiKey.find_by!(key: key)
-      end
-
-      def self.call(key)
-        new(key).query_params
+      def self.call(api_key)
+        new(api_key).query_params
       end
 
       def query_params
-        params = grouped_filters
+        params = api_response
 
         search_conditions = SEARCH_CONDITIONS_KEYS.map do |key|
           if value = params.delete(key)
@@ -32,16 +28,15 @@ module ClprApi
         }
       end
 
-      def grouped_filters
-        @grouped_filters ||= begin
-          _grouped_filters = api_key.api_key_filters.group_by(&:field)
+      private
 
-          _grouped_filters.map do |(key, filters)|
-            { key => filters.map(&:value).map(&:parameterize) }
-          end.inject({}) do |hash, set|
-            hash[set.keys.first] = set.values.flatten
-            hash
-          end.merge(has_photos: true)
+      def api_url
+        ENV.fetch("CONSOLE_API")
+      end
+
+      def api_response
+        @api_response ||= ClprApi.cache.fetch("API-KEY-CONFIGURATION-FOR-#{api_key}", expires_in: 1.hour) do
+          HTTParty.get("#{api_url}/api_keys/#{api_key}").parsed_response
         end
       end
 
---
+++ spec/lib/ClprApi/listings/filter_builder_by_api_key_spec.rb
@@ -0,0 +1,31 @@
+require "spec_helper"
+
+RSpec.describe ClprApi::Listings::FilterBuilderByApiKey do
+  let(:api_key) { "787POOONCEEEmonSUcasa321" }
+  let(:expected_response) do
+    {
+      params: {
+        "car_make" => [
+          "jeep",
+        ],
+        "car_model" => [
+          "wrangler", "wrangler-unlimited", "wrangler-sport", "wrangler", "wrangler-unlimited", "wrangler-sport", "wrangler-unlimited-sport", "wrangler-sahara", "wrangler-unlimited-sport", "wrangler-rubicon", "wrangler-rubicon", "wrangler-sport-unlimited", "wrangler-sport-unlimited", "wrangler-sport", "rubicon-unlimited", "rubicon-unlimited", "rubicon",
+        ],
+        "offering" => [
+          "sale",
+        ],
+        "category" => [
+          "vehiculos-carros",
+        ],
+        "has_photos" => true,
+      },
+      search_conditions: [
+        "lister_id_i:(20635)",
+      ],
+    }
+  end
+
+  it "requests the api and gets the search filters for a given API KEY" do
+    expect(described_class.call(api_key)).to eq(expected_response)
+  end
+end
