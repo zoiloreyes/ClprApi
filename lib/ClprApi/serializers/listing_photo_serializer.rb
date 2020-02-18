67c342deb588ed0ef251bdce1d11c42c17064aea
Marcos Mercedes <marcos.mercedesn@gmail.com>
2020-02-18 16:11:04 -0400

    Add cropped images resolutions

--- lib/ClprApi/serializers/listing_photo_serializer.rb
+++ lib/ClprApi/serializers/listing_photo_serializer.rb
@@ -7,7 +7,7 @@ module ClprApi
         @image = image
       end
 
-      IMAGES_VERSIONS = [:small, :medium, :large, :full].freeze
+      IMAGES_VERSIONS = [:small, :medium, :large, :large_cropped, :full, :full_cropped].freeze
 
       IMAGES_VERSIONS.each do |method_name|
         define_method(method_name) do
@@ -18,7 +18,9 @@ module ClprApi
       def versions
         @versions ||= Support::ThumborImage.new(self, :image) { |has|
           has.version :full, width: 1024, height: 768, fit_in: true
+          has.version :full_cropped, width: 1024, height: 768
           has.version :large, width: 640, height: 480, fit_in: true
+          has.version :large_cropped, width: 640, height: 480
           has.version :medium, width: 320, height: 240
           has.version :small, width: 160, height: 120
         }.image
